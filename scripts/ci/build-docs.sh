af21f0eaf89429bd2a11cb9ea4d01cbaaeadd656
Anas Nashif <anas.nashif@intel.com>
2017-04-16 19:17:29 -0400

    build docs
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/ci/build-docs.sh
+++ scripts/ci/build-docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+log_file=doc.log
+
 echo "building docs"
 sudo apt-get install doxygen make
 sudo pip install breathe sphinx
@@ -7,4 +9,6 @@ sudo pip install breathe sphinx
 cd ${ZEPHYRREPO_STATE}
 source zephyr-env.sh
 cd doc
-make htmldocs
+make htmldocs > "$log_file" 2>&1
+../scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc.log > doc.warnings
+test -s doc.warnings && cat doc.warnings && exit 1
