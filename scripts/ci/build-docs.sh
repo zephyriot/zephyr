9b66d8e4319ea5668bf8c0d10b1feda4217ba1bd
Anas Nashif <anas.nashif@intel.com>
2017-04-16 20:01:50 -0400

    fix doc script
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/ci/build-docs.sh
+++ scripts/ci/build-docs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-log_file=doc.log
-
-echo "building docs"
 sudo apt-get install doxygen make
 sudo pip install breathe sphinx
 
 cd ${ZEPHYRREPO_STATE}
 source zephyr-env.sh
 cd doc
-make htmldocs > "$log_file" 2>&1
-../scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc.log > doc.warnings
-test -s doc.warnings && cat doc.warnings && exit 1
+echo "- Building docs..."
+make htmldocs > doc.log 2>&1
+cd ..
+echo "- Look for new warnings..."
+./scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc/doc.log > doc.warnings
+cat doc.warnings
+test -s doc.warnings && exit 1
+
