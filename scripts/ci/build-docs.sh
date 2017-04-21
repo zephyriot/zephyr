6a7db058e614eed241650a6fb4d50e9f3ccfccc3
Anas Nashif <anas.nashif@intel.com>
2017-04-21 18:16:56 -0400

    run checkpatch
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/ci/build-docs.sh
+++ scripts/ci/build-docs.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
-exit 0
-sudo apt-get install doxygen make
-sudo pip install breathe sphinx
+set -e
 
 cd ${ZEPHYRREPO_STATE}
 source zephyr-env.sh
+git diff ${ZEPHYR_CIREPO_VERSIONNAME} | ${ZEPHYR_BASE}/scripts/checkpatch.pl --mailback --no-tree
+
+echo "- Install dependencies"
+sudo apt-get install doxygen make
+sudo pip install breathe sphinx
+
 echo "- Building docs..."
 make htmldocs > doc.log 2>&1
 echo "- Look for new warnings..."
-./scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc.log > doc.warnings
-cat doc.warnings
-test -s doc.warnings && exit 0 # FIXME
+#./scripts/filter-known-issues.py --config-dir .known-issues/doc/ doc.log > doc.warnings
+#cat doc.warnings
+#test -s doc.warnings && exit 0 # FIXME
 
